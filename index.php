0b883daa418589a96be77cca3c57a94f8c714373
abelhpp <abelipes@gmail.com>
2023-10-10 11:45:03 -0300

    EstructuraBase

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php include 'core/init.php'; ?>
+<?php include 'views/home.php'; ?>
+
+<?php
+    //Ejemplo de un ingreso en usuarios
+    
+    require_once 'models/usuarioModel.php';
+    //Importo model
+    
+    $usuarioModel = new UsuarioModel();
+    $result = $usuarioModel->registrarNuevoUsuario(31121592, 'abel humberto', 'paz','1234','abelipes@gmail.com', '2023-12-21', 'dni8', 1,1);
+    echo 'enviado';
+    echo $result;
+?>

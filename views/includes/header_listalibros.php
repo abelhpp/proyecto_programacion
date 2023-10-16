026776fbf9812ddec7da055da11e55ce0ae425f9
facundoR2 <facundo_rios1@hotmail.com>
2023-10-16 19:53:06 -0300

    agregado de footer y header para listalibros.php

--- views/includes/header_listalibros.php
+++ views/includes/header_listalibros.php
@@ -8,6 +8,7 @@
         integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <link rel="stylesheet" href="./assets/css/style_inicio.css">
+    <link rel="stylesheet" href="./assets/css/style_listalibros.css">
     <title>Biblioteca</title>
 </head>
 

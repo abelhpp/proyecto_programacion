d75a29741aa51709ed9849c4cca713af4bcaa721
facundoR2 <facundo_rios1@hotmail.com>
2023-10-16 19:50:07 -0300

    se agrega stilo para footer y header

--- views/includes/footerinicio.php
+++ views/includes/footerinicio.php
@@ -32,6 +32,9 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="./assets/js/inicio.js"></script>
+
+
+
 </body>
 
 </html>
